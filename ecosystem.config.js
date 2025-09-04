b9b76cf39f550a41356c6234c10cda3ebb4bf8c4
johan <maxtortecnorepaciones@gmail.com>
2025-09-04 17:57:46 -0500

    final

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,18 @@
 module.exports = {
-  apps : [
+  apps: [
     {
       name: "MundoHeladosAPI",
       script: "manage.py",
       args: ["runserver", "127.0.0.1:8001"],
       interpreter: "python",
-      cwd: "C:/Users/Administrador/Documents/Mundoherlados/",
-      wait_ready: true,
-      listen_timeout: 10000,
+      cwd: "C:/Users/Administrador/Documents/Mundoherlados/inventario_wasap/",
+      // Eliminamos wait_ready y listen_timeout
     },
     {
       name: "MundoHeladosBot",
       script: "index.js",
       cwd: "C:/Users/Administrador/Documents/Mundoherlados/bot-wasap/",
-      wait_ready: true,
-      listen_timeout: 10000,
-    }
-  ]
+      // Eliminamos wait_ready y listen_timeout
+    },
+  ],
 };
